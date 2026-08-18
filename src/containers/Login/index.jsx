3966d7d3b130bca350d0aba310716ebc31427d1c
ChristianPinho <christianpinho@outlook.com.br>
2026-08-17 21:09:28 -0300

    Button

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,9 +1,29 @@
-
+import { Container, LeftContainer, RightContainer, Title, Form, InputContainer } from './styles'
+import Logo from '../../assets/logo.svg'
+import { Button } from '../../components/Button'
 
 export function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Container>
+            <LeftContainer>
+                <img src={Logo} alt="Logo" />
+            </LeftContainer>
+            <RightContainer>
+                <Title>Olá, seja bem vindo ao <span>Dev Burguer!</span>
+                    <br />
+                    Acesse com seu <span>Login e senha.</span></Title>
+
+                <form>
+                    <InputContainer>
+                        <label>Email</label>
+                        <input type="email" placeholder="Digite seu email" />
+                        <label>Senha</label>
+                        <input type="password" placeholder="Digite sua senha" />
+                    </InputContainer>
+                    <Button>Entrar</Button>
+                </form>
+                <p>Não tem uma conta? <a>Clique aqui.</a></p>
+            </RightContainer>
+        </Container >
     )
 }
