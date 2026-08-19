9f04b145327d6676665c3e302258d15da5170a87
ChristianPinho <christianpinho@outlook.com.br>
2026-08-18 21:10:55 -0300

    Validation

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,8 +1,25 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
 import { Container, LeftContainer, RightContainer, Title, Form, InputContainer } from './styles'
 import Logo from '../../assets/logo.svg'
 import { Button } from '../../components/Button'
 
 export function Login() {
+
+    const schema = yup.object({
+        email: yup.string().email('Digite um email válido').required('O email é obrigatório'),
+        password: yup.string().min(6, 'A senha deve ter no mínimo 6 caracteres').required('A senha é obrigatória'),
+    }).required()
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
         <Container>
             <LeftContainer>
@@ -13,14 +30,19 @@ export function Login() {
                     <br />
                     Acesse com seu <span>Login e senha.</span></Title>
 
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <InputContainer>
                         <label>Email</label>
-                        <input type="email" placeholder="Digite seu email" />
-                        <label>Senha</label>
-                        <input type="password" placeholder="Digite sua senha" />
+                        <input type="email" placeholder="Digite seu email" {...register('email')} />
+                        <p>{errors?.email?.message}</p>
                     </InputContainer>
-                    <Button>Entrar</Button>
+
+                    <InputContainer>
+                        <label>Senha</label>
+                        <input type="password" placeholder="Digite sua senha" {...register('password')} />
+                        <p>{errors?.password?.message}</p>
+                    </InputContainer>
+                    <Button type="submit">Entrar</Button>
                 </form>
                 <p>Não tem uma conta? <a>Clique aqui.</a></p>
             </RightContainer>
