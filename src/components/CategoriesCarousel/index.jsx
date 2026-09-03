003862744083b07df64e41a3797c4ced66fa6a79
ChristianPinho <christianpinho@outlook.com.br>
2026-09-02 21:53:01 -0300

    adicionado carrossel

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,23 +1,54 @@
 import { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import { api } from '../../services/api';
+import { Container, Title, ContainerItems } from './styles';
 
 export function CategoriesCarousel() {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        async function loadCategories() {
-            const { data } = await api.get('/categories');
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories');
 
-            setCategories(data);
-            console.log(data);
-        }
+      setCategories(data);
+      console.log(data);
+    }
 
-        loadCategories();
-    }, []);
+    loadCategories();
+  }, []);
 
-    return (
-        <div>
-            <h1>Categories Carousel</h1>
-        </div>
-    );
+  return (
+    <Container>
+      <Title>Categorias</Title>
+
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={4}
+        navigation
+        modules={[Navigation]}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        }}
+      >
+        {categories.map((category) => (
+          <SwiperSlide key={category.id}>
+            <ContainerItems $imageUrl={category.url}>
+              <p>{category.name}</p>
+            </ContainerItems>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Container>
+  );
 }
